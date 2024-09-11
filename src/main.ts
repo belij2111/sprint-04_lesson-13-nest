d4272aefffbdc55b8ccc88b0c9c19c51fd4d6808
belij2111 <belijvalerij2111@gmail.com>
2024-09-11 22:48:44 +0300

    feat: Add CORS support to the app

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(3003);
 }
 
